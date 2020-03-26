1f9ea19d3365ad07000beb73271b9eafc6735bb8
Sherzod Nimatullo <shermanjones99@gmail.com>
2020-03-26 17:30:24 -0400

    styled credit link

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -17,7 +17,12 @@ const About = () => {
           <p>I am a computer science student from NYC.</p>
           <p>
             The design for this website was highly inspired by{" "}
-            <a style={{textDecoration="underline"}}target="_blank" href="https://www.instagram.com/dhanishgajjar/">
+            <a
+              style={{ textDecoration: "underline" }}
+              id="creditLink"
+              target="_blank"
+              href="https://www.instagram.com/dhanishgajjar/"
+            >
               Dhanish Gajjar.
             </a>
           </p>
