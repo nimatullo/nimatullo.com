6d6579cf6420532182507291220f8e46056566dc
Sherzod <shermanjones99@gmail.com>
2021-03-01 10:55:15 -0500

    added new memes 😎

--- src/components/GridChildExternal.jsx
+++ src/components/GridChildExternal.jsx
@@ -3,12 +3,22 @@ import { Link } from "gatsby";
 
 const GridChildExternal = ({ title, description }) => {
   const [memeLink, setMemeLink] = useState(0);
+  const [memeIndex, setIndex] = useState(0);
   const listOfMemes = [
+    "https://i.imgur.com/ijhBu3H.png",
+    "https://i.imgur.com/RYmZSao.png",
+    "https://i.imgur.com/FoamaYm.png",
     "https://i.imgur.com/4BMb9RY.png",
     "https://i.imgur.com/L75Dcvr.png",
     "https://i.imgur.com/IOu8cMH.jpg",
     "https://i.imgur.com/ZoUIp2U.png",
     "https://i.imgur.com/KAxRiuD.jpg",
+    "https://i.imgur.com/Nz8FuGC.png",
+    "https://i.imgur.com/SsAKh1L.jpg",
+    "https://i.imgur.com/qSkfhO1.jpg",
+    "https://i.imgur.com/hQBbiSt.png",
+    "https://i.imgur.com/QpMRSrx.png",
+    "https://i.imgur.com/jLXveVw.jpg",
     "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Are-you-a-robot-Meme-1024x925.jpg",
     "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Internet-Explorer-Joke-915x1024.jpg",
     "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Mark-Zuckerberg-Meme-878x1024.jpg",
@@ -17,12 +27,18 @@ const GridChildExternal = ({ title, description }) => {
     "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Google-Joke-1024x956.jpg",
   ];
 
-  useEffect(() => {
-    setMemeLink(listOfMemes[Math.floor(Math.random() * listOfMemes.length)]);
-  }, []);
-
   return (
-    <Link target="_blank" to={memeLink} className="gridChildContainer hover">
+    <a
+      onClick={() => {
+        memeIndex === listOfMemes.length - 1
+          ? setIndex(0)
+          : setIndex(memeIndex + 1);
+        setMemeLink(listOfMemes[memeIndex]);
+      }}
+      target="_blank"
+      href={memeLink}
+      className="gridChildContainer hover"
+    >
       <div>
         <header>
           <h4>{title}</h4>
@@ -30,7 +46,7 @@ const GridChildExternal = ({ title, description }) => {
         </header>
         <p>{description}</p>
       </div>
-    </Link>
+    </a>
   );
 };
 
