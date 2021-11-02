2cba9d9c808e5a18329d454ff31c7bac3d41c9ee
Sherzod <shermanjones99@gmail.com>
2021-11-02 08:44:56 -0400

    updated social media icons and added new playlist

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -13,12 +13,16 @@ const Playlist = () => {
         source="https://embed.music.apple.com/us/playlist/consensual-seduction/pl.u-jV890pJu3ooPqB"
       />
       <PlaylistEntry
-        title="summer 21"
+        title="new friends"
         source="https://embed.music.apple.com/us/playlist/summer-21/pl.u-KVXBD71Idoopm0"
       />
       <PlaylistEntry
         title="Kaleidoscopic"
         source="https://embed.music.apple.com/us/playlist/soft-porn/pl.u-xlyNqrlteKKZpv"
+      />
+      <PlaylistEntry
+        title="fall 21"
+        source="https://embed.music.apple.com/us/playlist/fall-21/pl.u-8aAVXEeIr77EaJ"
       />
     </>
   );
