d95977362a19d6d67533f7f6ac20f383b55abc47
Sherzod <shermanjones99@gmail.com>
2022-07-23 15:59:55 -0400

    resorting to a library...

--- src/api/a.js
+++ src/api/a.js
@@ -1,29 +1,30 @@
 const { addLink } = require("../services/firestore");
 const { parseHTML } = require("linkedom");
 const fetch = require("cross-fetch");
+const getTitleAtUrl = require("get-title-at-url");
 
 function isUrl(url) {
   const regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
   return regex.test(url);
 }
 
-async function getTitle(url) {
-  return fetch(`https://api.allorigins.win/get?url=${url}`)
-    .then((res) => res.text())
-    .then((body) => {
-      const html = parseHTML(body);
-      console.log("parseHTML result", html);
-      const title = html.querySelector("title");
-      console.log("Query selector result", title);
+// async function getTitle(url) {
+//   return fetch(`https://api.allorigins.win/get?url=${url}`)
+//     .then((res) => res.text())
+//     .then((body) => {
+//       const html = parseHTML(body);
+//       console.log("parseHTML result", html);
+//       const title = html.querySelector("title");
+//       console.log("Query selector result", title);
 
-      if (title) {
-        return title.innerText;
-      } else {
-        return "Cool little title";
-      }
-    })
-    .catch((err) => "Cool little title");
-}
+//       if (title) {
+//         return title.innerText;
+//       } else {
+//         return "Cool little title";
+//       }
+//     })
+//     .catch((err) => "Cool little title");
+// }
 
 // URL Format: https://nimatullo.com/api/a?u=https://www.google.com
 export default async function handler(req, res) {
@@ -35,19 +36,16 @@ export default async function handler(req, res) {
     return;
   }
 
-  console.log("URL", url);
-
   if (!url || !isUrl(url)) {
     res.status(400).json({
       message: "Invalid URL",
     });
   } else {
-    const title = await getTitle(url);
-
-    await addLink(url, title);
-
-    res.status(200).json({
-      message: `${url} added to the list`,
+    const title = getTitleAtUrl(url, (title) => {
+      addLink(url, title);
+      res.status(200).json({
+        message: `${url} added to the list`,
+      });
     });
   }
 }
